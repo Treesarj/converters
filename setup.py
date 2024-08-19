7f7e9b3999e2c5fb1088562656129bff10153f97
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2024-08-19 15:06:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name='salinity_converter',
-    version='0.1',
-    packages=find_packages(),
-    description='A package for converting between different salinity scales.',
-    author='Your Name',
-    author_email='your.email@example.com',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+    name="mypackage",  # Name of your package
+    version="0.1.0",  # Initial version
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[  # Dependencies of your package
+        # Example: 'requests',
     ],
-    python_requires='>=3.6',
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/yourusername/mypackage",  # URL to the package's source code
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
 )
