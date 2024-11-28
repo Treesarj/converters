971d7d44d47db6dfac84ce58aad4bd77cf82e943
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2024-11-28 14:41:19 +0100

    Update util.py

--- mymath/util.py
+++ mymath/util.py
@@ -10,31 +10,12 @@ def to_knudsen(v):
 def average_vector(vector):
     return np.mean(vector)
 
-def calc_cell_center_depth(blanking, cell_size, sensor_depth, df_north, df_east, df_w):
-    for df_ in [df_north, df_east, df_w]:
-        df_.columns = [
-            sensor_depth
-            + blanking
-            + (cell_size / 2)
-            + (c * cell_size)
-            for c in df_.columns
-        ]
-    return {
-        "df_north": df_north,
-        "df_east": df_east,
-        "df_w": df_w
-    }
-
-def calc_cell_center_depth1(blanking, cell_size, sensor_depth, df_north, df_east, df_w):
-    for df_ in [df_north, df_east, df_w]:
-        if not isinstance(df_, pd.DataFrame):
-            df_ = pd.DataFrame(df_)
-        
-        df_.columns = [
-            sensor_depth
-            + blanking
-            + (cell_size / 2)
-            + (c * cell_size)
-            for c in range(len(df_.columns))
-        ]
-    return  df_north
+def calc_cell_center_depth(blanking, cell_size, sensor_depth, velocity):
+    velocity = [
+        sensor_depth
+        + blanking
+        + (cell_size / 2)
+        + (c * cell_size)
+        for c in range(len(velocity))
+    ]
+    return velocity
