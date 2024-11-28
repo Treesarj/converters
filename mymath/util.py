8002de1ed32c5d725d07e6b177f32e57788125b1
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2024-11-28 09:43:53 +0100

    Update util.py

--- mymath/util.py
+++ mymath/util.py
@@ -8,3 +8,18 @@ def to_knudsen(v):
 
 def average_vector(vector):
     return np.mean(vector)
+
+def calc_cell_center_depth(blanking, cell_size, sensor_depth, df_north, df_east, df_w):
+    for df_ in [df_north, df_east, df_w]:
+        df_.columns = [
+            sensor_depth
+            + blanking
+            + (cell_size / 2)
+            + (c * cell_size)
+            for c in df_.columns
+        ]
+    return {
+        "df_north": df_north,
+        "df_east": df_east,
+        "df_w": df_w
+    }
