712c69bb655cae46448b189f64eb55ffe674924c
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2024-11-28 15:35:13 +0100

    Update util.py

--- mymath/util.py
+++ mymath/util.py
@@ -11,12 +11,17 @@ def average_vector(vector):
     return np.mean(vector)
 
 def calc_cell_center_depth(blanking, cell_size, sensor_depth, velocity):
-    velocity = np.array(velocity)
-    velocity = [
-        sensor_depth
-        + blanking
-        + (cell_size / 2)
-        + (c * cell_size)
-        for c in range(len(velocity))
-    ]
+    # Ensure velocity is a list or array
+    if isinstance(velocity, (np.ndarray, list)):
+        # Calculate new velocity values
+        velocity = [
+            sensor_depth
+            + blanking
+            + (cell_size / 2)
+            + (c * cell_size)
+            for c in range(len(velocity))
+        ]
+    else:
+        raise TypeError(f"Expected velocity to be a list or ndarray, got {type(velocity)} instead.")
+    
     return velocity
