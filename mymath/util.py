6ca46d0cfb075401ced856b0317e45bd10552931
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2024-11-29 08:58:04 +0100

    Update util.py

--- mymath/util.py
+++ mymath/util.py
@@ -56,6 +56,32 @@ def calc_cell_center_depth11(velocity):
         ]   
     return velocity
 
+def calc_cell_center_depth111(blanking,cell_size,sensor_depth,velocity):
+    blanking = 1
+    cell_size = 0.5
+    sensor_depth = 5
+    for df_ in [velocity]:
+        df_.columns = [
+            sensor_depth
+            + blanking
+            + (cell_size / 2)
+            + (c * cell_size)
+            for c in range(len(df_.columns))
+        ]   
+    return velocity
 
+def calc_cell_center_depth1111(blanking,cell_size,sensor_depth,velocity):
+    blanking = blanking
+    cell_size = cell_size
+    sensor_depth = sensor_depth
+    for df_ in [velocity]:
+        df_.columns = [
+            sensor_depth
+            + blanking
+            + (cell_size / 2)
+            + (c * cell_size)
+            for c in range(len(df_.columns))
+        ]   
+    return velocity
 
 
